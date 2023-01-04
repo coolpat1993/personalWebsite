c87552ff786ff96a895496e9ce449138738603ff
Patrick Shepherd <patrickshepherd218@gmail.com>
2023-01-04 19:29:16 +0000

    added basic state, made bool button

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,13 +2,20 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function NavBar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+
   return (
     <>
       <nav className="navBar">
         <div className="navBar-container">
-            <Link to="/" className="navBar-logo">
-                TRVL
-            </Link>
+          <Link to="/" className="navBar-logo">
+            Patrick S <i className="fab fa-typo3" />
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
         </div>
       </nav>
     </>
