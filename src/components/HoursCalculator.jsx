2a444afdba2a70993c47b2814134046215e4e3c0
Patrick Shepherd <patrickshepherd218@gmail.com>
2023-01-10 20:23:37 +0000

    oh my christ finally i got a build to work

--- src/components/HoursCalculator.jsx
+++ src/components/HoursCalculator.jsx
@@ -1,5 +1,13 @@
 import React, { useState } from "react";
+import { Document, Page, pdfjs } from "react-pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
+
+const options = {
+  cMapUrl: 'cmaps/',
+  cMapPacked: true,
+  standardFontDataUrl: 'standard_fonts/',
+};
 
 function calculateHours(hoursString) {
   let hours = hoursString.trim().split(" ");
@@ -16,7 +24,16 @@ function calculateHours(hoursString) {
 
 
 function HoursCalculator() {
+  const [file, setFile] = useState('./sample.pdf');
+  const [numPages, setNumPages] = useState(null);
 
+  function onFileChange(event) {
+    setFile(event.target.files[0]);
+  }
+
+  function onDocumentLoadSuccess({ numPages: nextNumPages }) {
+    setNumPages(nextNumPages);
+  }
 
   const [hoursString, setHoursString] = useState("");
   const [totalHours, setTotalHours] = useState(0);
@@ -41,6 +58,24 @@ function HoursCalculator() {
         <button type="submit">Calculate</button>
         <p>Total hours: {totalHours}</p>
       </form>
+      <div className="Example">
+        <header>
+          <h1>react-pdf sample page</h1>
+        </header>
+        <div className="Example__container">
+          <div className="Example__container__load">
+            <label htmlFor="file">Load from file:</label>{' '}
+            <input onChange={onFileChange} type="file" />
+          </div>
+          <div className="Example__container__document">
+            <Document file={file} onLoadSuccess={onDocumentLoadSuccess} options={options}>
+              {Array.from(new Array(numPages), (el, index) => (
+                <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+              ))}
+            </Document>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
